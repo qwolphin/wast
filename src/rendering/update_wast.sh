0e38e1fb777f9f2288905ab322fb547005c26eb8
Wolphin <q@wolph.in>
2022-10-23 20:12:54 +0500

    Updating wast

--- src/rendering/update_wast.sh
+++ src/rendering/update_wast.sh
@@ -4,7 +4,4 @@ cd "$(dirname "$0")"
 git add . && git commit -m 'Updating wast' && git push
 
 set -e
-mkdir tmp_wast_dir
-cp -r dev/* tmp_wast_dir
-
-mv tmp_wast_dir stable
+rsync -avh dev/ stable/ --delete
